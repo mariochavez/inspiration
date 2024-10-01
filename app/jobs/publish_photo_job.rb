a55591b3cb4b1e5e0312701f2c717c6e7bb76075
Mario Alberto Chávez <mario.chavez@gmail.com>
2024-10-01 15:25:10 -0600

    Hotfix, cambio para que Job continue

--- app/jobs/publish_photo_job.rb
+++ app/jobs/publish_photo_job.rb
@@ -3,7 +3,7 @@ class PublishPhotoJob < ApplicationJob
 
   def perform
     last_published_at = Photo.published.last&.published_at
-    return if Time.zone.now.hour.between?(9, 18) && (last_published_at && last_published_at < 2.hours.ago)
+    return unless Time.zone.now.hour.between?(9, 18) && (last_published_at && last_published_at < 2.hours.ago)
 
     photo = Photo.unpublished.order(created_at: :asc).first
     return unless photo
