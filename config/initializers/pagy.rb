6680517f1ee1026f7c954f907b6c8dbec2e02ef2
Mario Alberto Chávez <mario.chavez@gmail.com>
2024-10-17 21:04:46 -0600

    Actualiza el default de items de pagy

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -9,7 +9,7 @@
 # You can set any pagy variable as a Pagy::DEFAULT. They can also be overridden per instance by just passing them to
 # Pagy.new|Pagy::Countless.new|Pagy::Calendar::*.new or any of the #pagy* controller methods
 # Here are the few that make more sense as DEFAULTs:
-Pagy::DEFAULT[:items] = 5                    # default
+Pagy::DEFAULT[:items] = 10                    # default
 # Pagy::DEFAULT[:size]        = [1,4,4,1]             # default in pagy < 7.0
 # Pagy::DEFAULT[:page_param]  = :page                 # default
 # Pagy::DEFAULT[:count_args]  = []                    # example for non AR ORMs
